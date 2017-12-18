06132caba5dbfb8476bb46e6a22e4b2572b6672e
Lukas M <moser.lux@gmail.com>
2017-12-18 18:55:20 +0100

    Andere Werte für den Test mit der Rungefkt

--- Proj1/B/Test_Runge.m
+++ Proj1/B/Test_Runge.m
@@ -1,40 +1,48 @@
 syms x;
 f = symfun(1/(1+x^2), x);
-
-line = linspace(-5,5);
+a = -20;
+b = 20;
+line = linspace(a,b);
 f1 = f(line);
+n = 30;
 
 [samplePts1, forget] = GaussLegendre(1);
-[samplePts1, forget] = Transformation(samplePts1, forget, [-5 5]);
+[samplePts1, forget] = Transformation(samplePts1, forget, [a b]);
 [samplePts2, forget] = GaussLegendre(2);
-[samplePts2, forget] = Transformation(samplePts2, forget, [-5 5]);
+[samplePts2, forget] = Transformation(samplePts2, forget, [a b]);
 [samplePts5, forget] = GaussLegendre(5);
-[samplePts5, forget] = Transformation(samplePts5, forget, [-5 5]);
+[samplePts5, forget] = Transformation(samplePts5, forget, [a b]);
 [samplePts10, forget] = GaussLegendre(10);
-[samplePts10, forget] = Transformation(samplePts10, forget, [-5 5]);
+[samplePts10, forget] = Transformation(samplePts10, forget, [a b]);
+[samplePts20, forget] = GaussLegendre(20);
+[samplePts20, forget] = Transformation(samplePts20, forget, [a b]);
+[samplePts30, forget] = GaussLegendre(30);
+[samplePts30, forget] = Transformation(samplePts30, forget, [a b]);
 
 p1 = polyfit(samplePts1, double(f(samplePts1)),1);
 p2 = polyfit(samplePts2, double(f(samplePts2)),2);
 p5 = polyfit(samplePts5, double(f(samplePts5)),5);
 p10 = polyfit(samplePts10, double(f(samplePts10)),10);
+p20 = polyfit(samplePts20, double(f(samplePts20)),20);
+p30 = polyfit(samplePts30, double(f(samplePts30)),30);
 
-for i = 1:10
-    integral(i) = Quadratur(i, [-5 5], f); 
+for i = 1:n
+    integral(i) = Quadratur(i, [a b], f); 
 end
 
 figure
 plot(line,f1)
 hold on
-plot(line,polyval(p1, line), '--')
-plot(line,polyval(p2, line), '--')
 plot(line,polyval(p5, line), '--')
 plot(line,polyval(p10, line), '--')
+plot(line,polyval(p20, line), '--')
+plot(line,polyval(p30, line), '--')
 hold off
-legend('Runge-Funktion','P1','P2','P5','P10')
+legend('Runge-Funktion','P5','P10','P20','P30')
 
 figure
 bar(integral)
 hold on
-plot(0:11,ones(1,12)*double(2.746801533890031721722543852889922297301999191799401617939))
-axis([0 11 -inf inf])
+plot(0:n+1,ones(1,n+2)*double(3.041675862145907715642630809209813121214615238528091472153))
+axis([0 n+1 -inf inf])
 hold off
